939815e88388ed8338b3625c552a8937d252ac68
KS25KS <kanavjeetsingh25@gmail.com>
2025-06-25 14:40:35 +0200

    test

--- checkout.js
+++ checkout.js
@@ -503,7 +503,8 @@ if (checkoutBtn) {
     addPointsForOrder();
 
     // --- Discord Webhook Notification ---
-    const webhookUrl = 'https://discord.com/api/webhooks/1387405582561968218/EFxefJp_t6v1k9-c0a8pqzwfKNCUe1JghU0m5cv70_2koC5tsabw7lGvGUkz8PNGxazS';
+const webhookUrl = 'https://discord.com/api/webhooks/1387409986438692924/RlfhclopWZ16jHVH8lGmpl88hR6sKak5AYgEESMtGpe2LM_mTt8gj-jxJGBVI-FDTa_2';
+
 const orderItems = cart.map(item => {
   const quantity = item.quantity || 1;
   const isFree = item.isFree === true;
